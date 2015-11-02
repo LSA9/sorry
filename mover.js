da1cb4633a57421e98f36a0c2a9081a9ffa48305
Aronson1 <LSA9@pitt.edu>
2015-11-02 09:06:34 -0500

    Finished board setup.  Started js dev.

---
+++ mover.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Aronson1 on 11/1/15.
+ */
+
+var currentId
+var currentx
+var currenty
+var start
+
+function getActiveId(e){
+    currentId = e.id
+    currentx = e.offsetLeft
+    currenty = e.offsetTop
+    inStart()
+}
+
+function inStart(){
+    if(currentx)
+}
